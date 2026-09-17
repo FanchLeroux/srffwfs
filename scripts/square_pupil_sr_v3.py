45d3330eb749b55d3f1e292218442aaa8cd2242d
fleroux <francois.leroux.pro@gmail.com>
2026-09-17 19:27:08 +0200

    I think I got what I wanted. Need to clarify the input frequencies forming a basis structure

--- scripts/square_pupil_sr_v3.py
+++ scripts/square_pupil_sr_v3.py
@@ -7,7 +7,6 @@ from srffwfs.fourier_basis import (
     draw_labels,
     get_labels,
     compute_fourier_basis,
-    rotate_fourier_labels,
 )
 from srffwfs.binning import bin_2d
 
@@ -69,6 +68,8 @@ fourier_basis_binned_4_flat = fourier_basis_binned_4.reshape(
     fourier_basis_binned_4.shape[0], -1
 ).T
 
+# %%
+
 fourier_basis_binned_1_flat_stacked = np.vstack(
     (
         fourier_basis_binned_1_flat,
@@ -114,7 +115,25 @@ fourier_basis_binned_all_flat_stacked = np.vstack(
     )
 )
 
-fourier_basis_binned_2_grids_flat_stacked = np.vstack(
+fourier_basis_binned_2_grids_flat_stacked_1 = np.vstack(
+    (
+        fourier_basis_binned_1_flat,
+        fourier_basis_binned_2_flat,
+        fourier_basis_binned_1_flat,
+        fourier_basis_binned_2_flat,
+    )
+)
+
+fourier_basis_binned_2_grids_flat_stacked_2 = np.vstack(
+    (
+        fourier_basis_binned_1_flat,
+        fourier_basis_binned_3_flat,
+        fourier_basis_binned_1_flat,
+        fourier_basis_binned_3_flat,
+    )
+)
+
+fourier_basis_binned_2_grids_flat_stacked_3 = np.vstack(
     (
         fourier_basis_binned_1_flat,
         fourier_basis_binned_4_flat,
@@ -130,15 +149,26 @@ u4, s4, vt4 = np.linalg.svd(fourier_basis_binned_4_flat_stacked, full_matrices=F
 uall, sall, vtall = np.linalg.svd(
     fourier_basis_binned_all_flat_stacked, full_matrices=False
 )
-u2g, s2g, vt2g = np.linalg.svd(
-    fourier_basis_binned_2_grids_flat_stacked, full_matrices=False
+u2g_1, s2g_1, vt2g_1 = np.linalg.svd(
+    fourier_basis_binned_2_grids_flat_stacked_1, full_matrices=False
+)
+u2g_2, s2g_2, vt2g_2 = np.linalg.svd(
+    fourier_basis_binned_2_grids_flat_stacked_2, full_matrices=False
+)
+u2g_3, s2g_3, vt2g_3 = np.linalg.svd(
+    fourier_basis_binned_2_grids_flat_stacked_3, full_matrices=False
+)
+u2g_3, s2g_3, vt2g_3 = np.linalg.svd(
+    fourier_basis_binned_2_grids_flat_stacked_3, full_matrices=False
 )
 
 print(f"cond1: {s1.max() / s1.min()}")
 print(f"cond2: {s2.max() / s2.min()}")
 print(f"cond3: {s3.max() / s3.min()}")
 print(f"cond4: {s4.max() / s4.min()}")
-print(f"cond2g: {s2g.max() / s2g.min()}")
+print(f"cond2g_1: {s2g_1.max() / s2g_1.min()}")
+print(f"cond2g_2: {s2g_2.max() / s2g_2.min()}")
+print(f"cond2g_3: {s2g_3.max() / s2g_3.min()}")
 print(f"condall: {sall.max() / sall.min()}")
 
 fig, axs = plt.subplots(2, 2, constrained_layout=True)
@@ -158,8 +188,16 @@ plt.semilogy(sall, marker="+")
 plt.title("Singular Values - Binned Fourier Basis All")
 
 fig = plt.figure()
-plt.semilogy(s2g, marker="+")
-plt.title("Singular Values - Binned Fourier Basis 2 Grids")
+plt.semilogy(s2g_1, marker="+")
+plt.title("Singular Values - Binned Fourier Basis 2 Grids 1")
+
+fig = plt.figure()
+plt.semilogy(s2g_2, marker="+")
+plt.title("Singular Values - Binned Fourier Basis 2 Grids 2")
+
+fig = plt.figure()
+plt.semilogy(s2g_3, marker="+")
+plt.title("Singular Values - Binned Fourier Basis 2 Grids 3")
 
 plt.show()
 
@@ -168,40 +206,100 @@ plt.show()
 fig, axs = plt.subplots(2, 2, constrained_layout=True)
 axs[0, 0].imshow(np.abs(vt1))
 axs[0, 0].set_title("V^T - Binned Fourier Basis 1")
-axs[0, 1].imshow(np.abs(vt2))
-axs[0, 1].set_title("V^T - Binned Fourier Basis 2")
-axs[1, 0].imshow(np.abs(vt2g))
-axs[1, 0].set_title("V^T - Binned Fourier Basis 2 Grids")
-axs[1, 1].imshow(np.abs(vtall))
-axs[1, 1].set_title("V^T - Binned Fourier Basis All")
+axs[0, 1].imshow(np.abs(vt2g_1))
+axs[0, 1].set_title("V^T - Binned Fourier Basis 2 Grids 1")
+axs[1, 0].imshow(np.abs(vt2g_2))
+axs[1, 0].set_title("V^T - Binned Fourier Basis 2 Grids 2")
+axs[1, 1].imshow(np.abs(vt2g_3))
+axs[1, 1].set_title("V^T - Binned Fourier Basis 2 Grids 3")
+
+plt.figure()
+plt.imshow(np.abs(vtall))
+plt.title("V^T - Binned Fourier Basis All")
 
 # %%
 
-eigen_modes_2g_flat = fourier_basis_flat @ vt2g.T
+eigen_modes_2g_1_flat = fourier_basis_flat @ vt2g_1.T
 
-eigen_modes_2g = eigen_modes_2g_flat.T.reshape(
+eigen_modes_2g_1 = eigen_modes_2g_1_flat.T.reshape(
     (fourier_basis.shape[0],) + fourier_basis.shape[1:]
 )
 
 eigen_mode_index = 0
 
 plt.figure()
-plt.imshow(eigen_modes_2g[eigen_mode_index])
-plt.title(f"Eigenmode {eigen_mode_index} - Binned Fourier Basis 2 Grids")
+plt.imshow(eigen_modes_2g_1[eigen_mode_index])
+plt.title(f"Eigenmode {eigen_mode_index} - Binned Fourier Basis 2 Grids 1")
+plt.show()
+
+eigen_modes_2g_2_flat = fourier_basis_flat @ vt2g_2.T
+
+eigen_modes_2g_2 = eigen_modes_2g_2_flat.T.reshape(
+    (fourier_basis.shape[0],) + fourier_basis.shape[1:]
+)
+
+eigen_mode_index = 0
+
+plt.figure()
+plt.imshow(eigen_modes_2g_2[eigen_mode_index])
+plt.title(f"Eigenmode {eigen_mode_index} - Binned Fourier Basis 2 Grids 2")
+plt.show()
+
+eigen_modes_2g_3_flat = fourier_basis_flat @ vt2g_3.T
+
+eigen_modes_2g_3 = eigen_modes_2g_3_flat.T.reshape(
+    (fourier_basis.shape[0],) + fourier_basis.shape[1:]
+)
+
+eigen_mode_index = 0
+
+plt.figure()
+plt.imshow(eigen_modes_2g_3[eigen_mode_index])
+plt.title(f"Eigenmode {eigen_mode_index} - Binned Fourier Basis 2 Grids 3")
 plt.show()
 
 # %%
 
 n_modes_2g = (n_sampling_points * 2) ** 2 // 2
-modes_weight_2g = (np.abs(vt2g[:n_modes_2g]) ** 2).sum(axis=0)
 
-first_modes_2g = np.argsort(modes_weight_2g)[-n_modes_2g:][::-1]
+modes_weight_2g_1 = (np.abs(vt2g_1[:n_modes_2g]) ** 2).sum(axis=0)
+first_modes_2g_1 = np.argsort(modes_weight_2g_1)[-n_modes_2g:][::-1]
 
 plt.figure()
-plt.plot(modes_weight_2g)
+plt.plot(modes_weight_2g_1)
 
-print(f"First {n_modes_2g} modes: {first_modes_2g}")
+print(f"First {n_modes_2g} modes: {first_modes_2g_1}")
 
-first_modes_2g_labels = [labels_sr[i] for i in first_modes_2g]
+first_modes_2g_1_labels = [labels_sr[i] for i in first_modes_2g_1]
 draw_labels(labels_sr)
-draw_labels(first_modes_2g_labels)
+draw_labels(first_modes_2g_1_labels)
+
+# %%
+
+modes_weight_2g_2 = (np.abs(vt2g_2[:n_modes_2g]) ** 2).sum(axis=0)
+first_modes_2g_2 = np.argsort(modes_weight_2g_2)[-n_modes_2g:][::-1]
+
+plt.figure()
+plt.plot(modes_weight_2g_2)
+
+print(f"First {n_modes_2g} modes: {first_modes_2g_2}")
+
+first_modes_2g_2_labels = [labels_sr[i] for i in first_modes_2g_2]
+draw_labels(labels_sr)
+draw_labels(first_modes_2g_2_labels)
+
+# %%
+
+modes_weight_2g_3 = (np.abs(vt2g_3[:n_modes_2g]) ** 2).sum(axis=0)
+first_modes_2g_3 = np.argsort(modes_weight_2g_3)[-n_modes_2g:][::-1]
+
+plt.figure()
+plt.plot(modes_weight_2g_3)
+
+print(f"First {n_modes_2g} modes: {first_modes_2g_3}")
+
+first_modes_2g_3_labels = [labels_sr[i] for i in first_modes_2g_3]
+draw_labels(labels_sr)
+draw_labels(first_modes_2g_3_labels)
+
+# %%
